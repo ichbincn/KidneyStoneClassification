401f1bb0f01ef68367ecf056597eedfcaa2a8f81
fancccc <workfc@163.com>
2023-10-31 11:19:20 +0800

    fanc commit dataset codes

---
+++ src/dataloader/data_transform.py
@@ -0,0 +1,104 @@
+# -*- coding: utf-8 -*-
+# Time    : 2023/10/29 15:26
+# Author  : fanc
+# File    : data_transform.py
+
+import os
+import numpy as np
+import pandas as pd
+import pydicom
+import nrrd
+import re
+import json
+import nibabel as nib
+def read_csv(file_path):
+    df = pd.read_csv(file_path, encoding='gb18030')
+    columns = list(df.columns)
+
+    df.columns = ['姓名', '编号', '结石成分'] + columns[3:]
+    return df[['姓名', '编号', '结石成分']]
+
+def load_dcm(dcm_dir):
+    dcm_list = os.listdir(dcm_dir)
+    dicom_files = [pydicom.dcmread(os.path.join(dcm_list, f)) for f in sorted(os.listdir(dcm_list), reverse=True)]
+    return dicom_files
+
+def load_nrrd(file_path):
+    data, header = nrrd.read(file_path)
+    return data
+
+def map_data(data_dir):
+    infos = []
+    # data_dir = r'C:\Users\Asus\Desktop\肺腺癌\data\肾结石数据\KdneyStone\202310326结石成分分析龙岗区人民医院李星智'
+    dcm_dir = os.path.join(data_dir, '结石原始图像')
+    nrrd_dir = os.path.join(data_dir, 'mask')
+    csv_dir = os.path.join(data_dir, 'list.csv')
+    # 读取csv文件
+    df = read_csv(csv_dir)
+    print(df)
+    names = os.listdir(dcm_dir)
+    for name in names:
+        print(name)
+        if re.search('\D+', name):
+            temp = df[df['姓名'] == re.search('\D+', name).group()].reset_index(drop=True)
+            print('temp', temp)
+            if len(temp) > 0:
+                for i in range(len(temp)):
+                    if temp.loc[i, '结石成分']:
+                        continue
+                    id_ = temp.loc[i, '编号']
+                    if not os.path.exists(os.path.join(nrrd_dir, id_ + '.nrrd')):
+                        continue
+                    seg_data = load_nrrd(os.path.join(nrrd_dir, id_ + '.nrrd'))
+                    seg_shape = seg_data.shape
+                    temp_path = os.path.join(dcm_dir, name)
+                    for j in os.listdir(temp_path):
+                        if len(os.listdir(os.path.join(temp_path, j))) == seg_shape[-1] and os.path.exists(os.path.join(nrrd_dir, id_ + '.nrrd')):
+                            dic = {'name': re.search('\D+', name).group(), 'id': id_, 'shape': seg_shape,
+                                   'label': int(temp.loc[i, '结石成分']),
+                                   'dcm_dir': os.path.join(temp_path, j),
+                                   'seg_dir': os.path.join(nrrd_dir, id_ + '.nrrd')}
+                            infos.append(dic)
+    print(len(infos))
+    with open(os.path.join(data_dir, 'infos.json'), 'w') as f:
+        json.dump(infos, f)
+
+def makedirs(dir):
+    if not os.path.exists(dir):
+        os.makedirs(dir)
+def data2nii(info_dir, save_dir):
+    makedirs(os.path.join(save_dir, 'imgs_nii'))
+    makedirs(os.path.join(save_dir, 'mask_nii'))
+    makedirs(os.path.join(save_dir, 'slices_npy'))
+    makedirs(os.path.join(save_dir, 'mask_npy'))
+    with open(os.path.join(info_dir, 'infos.json'), 'r') as f:
+        infos = json.load(f)
+    i = 1
+    for info in infos:
+        dcm_dir = info['dcm_dir']
+        seg_dir = info['seg_dir']
+        slices = [pydicom.dcmread(os.path.join(dcm_dir, i)) for i in os.listdir(dcm_dir)]
+        # Sort the dicom slices in their respective order
+        slices.sort(key=lambda x: int(x.InstanceNumber), reverse=True)
+        # Get the pixel values for all the slices
+        slices = np.stack([s.pixel_array.T for s in slices], axis=-1)
+
+        nifti_image = nib.Nifti1Image(slices, affine=None)
+        nib.save(nifti_image, os.path.join(save_dir, 'imgs_nii', f"{info['id']}.nii.gz"))
+        np.save(os.path.join(save_dir, 'slices_npy', f"{info['id']}.npy"), slices)
+
+        mask, header = nrrd.read(seg_dir)
+        # 102 210err np.flipud(mask)
+
+        img = nib.Nifti1Image(mask, np.eye(4))
+        nib.save(img, os.path.join(save_dir, 'mask_nii', f"{info['id']}-mask.nii.gz"))  # 保存 nii 文件
+        np.save(os.path.join(save_dir, 'mask_npy', f"{info['id']}-mask.npy"), mask)
+        print(f'完成进度：{i}/{len(infos)}--{i/len(infos)}')
+        i += 1
+
+if __name__ == '__main__':
+    # data_dir = r'C:\Users\Asus\Desktop\肺腺癌\data\肾结石数据\KdneyStone\202310326结石成分分析龙岗区人民医院李星智'
+    data_dir = '/home/fanchenchenzc/datasets/202310326结石成分分析龙岗区人民医院李星智'
+    map_data(data_dir)
+
+    data2nii(info_dir=data_dir, save_dir=data_dir)
---
+++ src/dataloader/load_data.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+# Time    : 2023/10/30 16:03
+# Author  : fanc
+# File    : load_data.py
+
+import os
+import numpy as np
+import random
+from torch.utils.data import DataLoader, Dataset
+import json
+from skimage.transform import resize
+
+def split_data(data_dir, rate=0.8):
+    with open(os.path.join(data_dir, 'infos.json'), 'r') as f:
+        infos = json.load(f)
+    random.seed(1900)
+    random.shuffle(infos)
+    num_samples = len(infos)
+    train_num = int(rate * num_samples)
+    test_num = num_samples - train_num
+    train_infos = infos[:train_num]
+    test_infos = infos[train_num:]
+
+    return train_infos, test_infos
+
+class MyDataset(Dataset):
+    def __init__(self, data_dir, infos, size=(256, 256, 256)):
+        slices_dir = os.path.join(data_dir, 'slices_npy')
+        mask_dir = os.path.join(data_dir, 'mask_npy')
+
+        self.size = size
+        self.slices_dir = slices_dir
+        self.mask_dir = mask_dir
+        self.labels = [i['label'] for i in infos]
+        self.ids = [i['id'] for i in infos]
+
+    def __len__(self):
+        return len(self.ids)
+
+    def __getitem__(self, i):
+        image = np.load(os.path.join(self.slices_dir, f"{self.ids[i]}.npy")).astype('float64')
+        mask = np.load(os.path.join(self.mask_dir, f"{self.ids[i]}-mask.npy")).astype('float64')
+        image, mask = self.preprocess(image, mask)
+        label = self.labels[i]
+        return image, mask, label
+    def preprocess(self, img, mask):
+        img = resize(img, self.size)
+        mask = resize(mask, self.size)
+        img = (img/255).astype(np.float32)
+        if np.min(img) < np.max(img):
+            img = img - np.min(img)
+            img = img / np.max(img)
+        return img, mask
+
+def my_dataloader(data_dir, infos, batch_size=3, shuffle=True, num_workers=0):
+    dataset = MyDataset(data_dir, infos)
+    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers)
+    return dataloader
+
+# data_dir = r'C:\Users\Asus\Desktop\肺腺癌\data\肾结石数据\KdneyStone\202310326结石成分分析龙岗区人民医院李星智'
+#
+# train_info, test_info = split_data(data_dir, rate=0.8)
+# train_dataloader = my_dataloader(data_dir, train_info)
+# test_dataloader = my_dataloader(data_dir, test_info)
+# for i, (image, mask, label) in enumerate(train_dataloader):
+#     print(i,  image.shape, mask.shape, label)
+#
+# for i, (image, mask, label) in enumerate(test_dataloader):
+#     print(i,  image.shape, mask.shape, label)
